6403d81ec9bca2409c189442b1cd0fbb8dc00471
George Legeza <glegeza@gmail.com>
2018-03-23 11:11:51 -0400

    Create Breathability enum

---
+++ starform/data/Breathability.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    None: Symbol('none'),
+    Breathable: Symbol('breathable'),
+    Unbreathable: Symbol('unbreathable'),
+    Poisonous: Symbol('poisonous'),
+});
