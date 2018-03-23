d31b1421c73b79b3509d7fc24b88ac55faeb849f
George Legeza <glegeza@gmail.com>
2018-03-23 12:34:48 -0400

    Port PlanetType enum

---
+++ starform/data/PlanetType.js
@@ -0,0 +1,13 @@
+module.exports = Object.freeze({
+    Unknown: Symbol('unknown'),
+    Barren: Symbol('barren'),
+    Venusian: Symbol('venusian'),
+    Terrestrial: Symbol('terrestrial'),
+    GasGiant: Symbol('gasGiant'),
+    Martian: Symbol('martian'),
+    Water: Symbol('water'),
+    Ice: Symbol('ice'),
+    SubGasGiant: Symbol('subGasGiant'),
+    SubSubGasGiant: Symbol('subSubGasGiant'),
+    Asteroids: Symbol('asteroids'),
+});
