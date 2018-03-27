ccd58ae0f05d46c64fc3fcabd94e928a10b2d94d
George Legeza <glegeza@gmail.com>
2018-03-27 13:56:52 -0400

    Add doc comments

--- starform/environment/hillSphere.js
+++ starform/environment/hillSphere.js
@@ -1,9 +1,21 @@
 const global = require('../globalConstants');
 
+/**
+ * @param {number} M the mass of the larger body
+ * @param {number} m the mass of the smaller body
+ * @param {number} a semi-major axis in AU
+ * @returns {number} the simplified hill sphere in KM
+*/
 const simplifiedHillSphereKm = (M, m, a) => {
     return simplifiedHillSphereAu(M, m, a) * global.KM_PER_AU;
 }
 
+/**
+ * @param {number} M the mass of the larger body
+ * @param {number} m the mass of the smaller body
+ * @param {a} semi-major axis in AU
+ * @returns {number} the simplified hill sphere in AU
+*/
 const simplifiedHillSphereAu = (M, m, a) => {
     return a * Math.pow(m / (3 * M), (1.0 / 3.0));
 }
--- starform/environment/rocheLimit.js
+++ starform/environment/rocheLimit.js
@@ -1,9 +1,23 @@
 const global = require('../globalConstants');
 
+/**
+ * @description Returns the Roche limit of an object in KM
+ * @param {number} bodyRadius Radius of the body in km
+ * @param {number} bodyDensity Density of the body in g/cc
+ * @param {number} satelliteDensity Density of the orbiting satellite in g/cc
+ * @returns {number} roche limit in KM
+ */
 const rocheLimitKm = (bodyRadius, bodyDensity, satelliteDensity) => {
     return (1.26 * bodyRadius * Math.pow(bodyDensity / satelliteDensity, 1.0 / 3.0)) / 1000.0;
 };
 
+/**
+ * @description Returns the Roche limit of an object in AU
+ * @param {number} bodyRadius Radius of the body in km
+ * @param {number} bodyDensity Density of the body in g/cc
+ * @param {number} satelliteDensity Density of the orbiting satellite in g/cc
+ * @returns {number} roche limit in AU
+ */
 const rocheLimitAu = (bodyRadius, bodyDensity, satelliteDensity) => {
     return rocheLimitKm(bodyRadius, bodyDensity, satelliteDensity)
         / global.KM_PER_AU;
