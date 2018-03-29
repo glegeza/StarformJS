c3acef6426945b853b1b04a303ec0739e607d855
George Legeza <glegeza@gmail.com>
2018-03-29 11:33:01 -0400

    Move tidally locked test data to json file

--- tests/starform/environment/planetQualities.test.js
+++ tests/starform/environment/planetQualities.test.js
@@ -1,26 +1,21 @@
 const environnment = require('../../../starform/environment/planetQualities');
+const testPlanets = require('./data/planetQualities.json');
 const expect = require('expect');
 
 describe('isTidallyLocked Tests', () => {
+    const locked = testPlanets.tidallyLocked;
+    const notLocked = testPlanets.notTidallyLocked;
+
     it('should return true if a planet\'s day and orbital period are exactly equal', () => {
-        const planet = {
-            day: 24,
-            orbitalPeriod: 1,
-        };
-        expect(environnment.isTidallyLocked(planet)).toBeTruthy();
+        locked.forEach((p) => {
+            expect(environnment.isTidallyLocked(p)).toBeTruthy();    
+        });
     });
 
     it('should return false if a planet\'s day and orbital period differ', () => {
-        const planet1 = {
-            day: 24,
-            orbitalPeriod: 365
-        };
-        const planet2 = {
-            day: 24,
-            orbitalPeriod: 1.5
-        };
-        expect(environnment.isTidallyLocked(planet1)).toBeFalsy();
-        expect(environnment.isTidallyLocked(planet2)).toBeFalsy();
+        notLocked.forEach((p) => {
+            expect(environnment.isTidallyLocked(p)).toBeFalsy();    
+        });
     });
 });
 
