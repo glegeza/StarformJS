8ea84bc31698cf8697c56893b606a676bb64cd6c
George Legeza <glegeza@gmail.com>
2018-03-29 13:44:49 -0400

    Export function

--- starform/environment/orbitalZone.js
+++ starform/environment/orbitalZone.js
@@ -2,10 +2,12 @@ const orbitalZone = (luminosity, orbRadius) => {
     if (orbRadius < (4.0 * Math.sqrt(luminosity))) {
         return 1;
     }
-    
+
     if (orbRadius < (15.0 * Math.sqrt(luminosity))) {
         return 2;
     }
 
     return 3;
 };
+
+module.exports = orbitalZone;
