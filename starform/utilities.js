d3335cd0a6463f5a152cc9cb58ab76dc1ac3a415
George Legeza <glegeza@gmail.com>
2018-03-31 03:20:33 -0400

    Add a few more utility funcs

--- starform/utilities.js
+++ starform/utilities.js
@@ -2,6 +2,14 @@ const pow2 = (a) => {
     return a * a;
 };
 
+const pow3 = (a) => {
+    return a * a * a;
+}
+
+const pow1_4 = (a) => {
+    return Math.sqrt(Math.sqrt(a));
+}
+
 module.exports = {
-    pow2,
+    pow2, pow3, pow1_4
 };
