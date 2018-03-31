23dd4bea19172f893476b2a78bd543c4d20419bb
George Legeza <glegeza@gmail.com>
2018-03-31 03:39:01 -0400

    Add stub for orbit functions

---
+++ starform/environment/orbit.js
@@ -0,0 +1,24 @@
+const GlobalConstants = require('../globalConstants');
+
+/**
+ * Returns a planet's period in Earth days
+ * @param {Number} separation Separation in units of AU
+ * @param {Number} smallMass Mass of the smaller body in units of solar masses
+ * @param {Number} largeMass Mass of the larger body in units of solar masses
+ */
+const period = (separation, smallMass, largeMass) => {
+
+};
+
+// TODO IMPORTANT: this modifies the planet and it shouldn't
+/**
+ * Returns the length of a planet's day in hours
+ * @param {Planet} planet The planet to calculate a 
+ */
+const dayLength = (planet) => {
+
+};
+
+module.exports = {
+    period, dayLength
+};
